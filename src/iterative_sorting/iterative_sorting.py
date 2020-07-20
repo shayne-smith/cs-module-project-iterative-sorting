8bb9d4bfd75491e5f83e257f6ba4971e0c35186d
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-20 16:59:36 -0500

    wip

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,5 @@
 # TO-DO: Complete the selection_sort() function below
+# O(n^2)
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -19,6 +20,7 @@ def selection_sort(arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
+# O(n^2)
 def bubble_sort(arr):
     # Your code here
     # compare LHS and RHS variables
