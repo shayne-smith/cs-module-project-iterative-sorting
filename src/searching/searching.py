dc6e13ff7394f0b995c559ffc8ea74a373a2bf22
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-20 16:58:56 -0500

    Implemented binary and linear search functions

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,17 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        mid = (high + low) // 2
+
+        if target == arr[mid]:
+            return mid
+        if target < arr[mid]:
+            high = mid - 1
+        if target > arr[mid]:
+            low = mid + 1
 
     return -1  # not found
